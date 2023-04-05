3804269c12c4c792381492e0076dff25d6a595de
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-04-05 10:03:35 -0300

    Init3

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
   // converts the time to format 0:00
-  const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
+  const getTime = (time) =>
+    `${Math.floor(time / 60)}:${`0${Math.floor(time % 60)}`.slice(-2)}`;
 
   return (
-    <div className="hidden sm:flex flex-row items-center">
-      <button type="button" onClick={() => setSeekTime(appTime - 5)} className="hidden lg:mr-4 lg:block text-white">
+    <div className="hidden flex-row items-center sm:flex">
+      <button
+        type="button"
+        onClick={() => setSeekTime(appTime - 5)}
+        className="hidden text-white lg:mr-4 lg:block"
+      >
         -
       </button>
-      <p className="text-white">{value === 0 ? '0:00' : getTime(value)}</p>
+      <p className="text-white">{value === 0 ? "0:00" : getTime(value)}</p>
       <input
         type="range"
         step="any"
@@ -17,10 +22,14 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
         min={min}
         max={max}
         onInput={onInput}
-        className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg"
+        className="mx-4 h-1 w-24 rounded-lg md:block md:w-56 2xl:mx-6 2xl:w-96"
       />
-      <p className="text-white">{max === 0 ? '0:00' : getTime(max)}</p>
-      <button type="button" onClick={() => setSeekTime(appTime + 5)} className="hidden lg:ml-4 lg:block text-white">
+      <p className="text-white">{max === 0 ? "0:00" : getTime(max)}</p>
+      <button
+        type="button"
+        onClick={() => setSeekTime(appTime + 5)}
+        className="hidden text-white lg:ml-4 lg:block"
+      >
         +
       </button>
     </div>
--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -1,45 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import PlayPause from './PlayPause';
+import PlayPause from "./PlayPause";
 
-const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
-    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
-    <div className="flex-1 flex flex-row justify-between items-center">
+const SongBar = ({
+  song,
+  i,
+  artistId,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => (
+  <div
+    className={`flex w-full flex-row items-center hover:bg-[#4c426e] ${
+      activeSong?.title === song?.title ? "bg-[#4c426e]" : "bg-transparent"
+    } mb-2 cursor-pointer rounded-lg p-4 py-2`}
+  >
+    <h3 className="mr-3 text-base font-bold text-white">{i + 1}.</h3>
+    <div className="flex flex-1 flex-row items-center justify-between">
       <img
-        className="w-20 h-20 rounded-lg"
-        src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart}
+        className="h-20 w-20 rounded-lg"
+        src={
+          artistId
+            ? song?.attributes?.artwork?.url
+                .replace("{w}", "125")
+                .replace("{h}", "125")
+            : song?.images?.coverart
+        }
         alt={song?.title}
       />
-      <div className="flex-1 flex flex-col justify-center mx-3">
+      <div className="mx-3 flex flex-1 flex-col justify-center">
         {!artistId ? (
           <Link to={`/songs/${song.key}`}>
-            <p className="text-xl font-bold text-white">
-              {song?.title}
-            </p>
+            <p className="text-xl font-bold text-white">{song?.title}</p>
           </Link>
         ) : (
           <p className="text-xl font-bold text-white">
             {song?.attributes?.name}
           </p>
         )}
-        <p className="text-base text-gray-300 mt-1">
+        <p className="mt-1 text-base text-gray-300">
           {artistId ? song?.attributes?.albumName : song?.subtitle}
         </p>
       </div>
     </div>
-    {!artistId
-      ? (
-        <PlayPause
-          isPlaying={isPlaying}
-          activeSong={activeSong}
-          song={song}
-          handlePause={handlePauseClick}
-          handlePlay={() => handlePlayClick(song, i)}
-        />
-      )
-      : null}
+    {!artistId ? (
+      <PlayPause
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        song={song}
+        handlePause={handlePauseClick}
+        handlePlay={() => handlePlayClick(song, i)}
+      />
+    ) : null}
   </div>
 );
 
