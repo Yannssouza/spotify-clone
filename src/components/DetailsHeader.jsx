fb858c9858626dbf0a43c1cdd870c8ee35b9ff1c
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-04-08 16:08:53 -0300

    "New"

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,3 +1,35 @@
-const DetailsHeader = () => <div>DetailsHeader</div>;
+import { Link } from "react-router-dom";
+
+const DetailsHeader = ({ artistId, artistData, songData }) => {
+  const artist = artistData?.artists[artistId]?.attributes;
+  return (
+    <div className="relative flex w-full flex-col">
+      <div className="h-28 w-full bg-gradient-to-l from-transparent to-black sm:h-48" />
+      <div className="absolute inset-0 flex items-center">
+        <img
+          alt="art"
+          src={
+            artistId
+              ? artist?.artwork?.url.replace("{w}", "500").replace("{h}", "500")
+              : songData?.images?.coverart
+          }
+          className="h-28 w-28 rounded-full border-2 object-cover shadow-xl shadow-black sm:h-48 sm:w-48"
+        />
+        <div className="ml-5">
+          <p className="text-xl font-bold text-white sm:text-3xl">
+            {artistId ? artist?.name : songData?.title}
+          </p>
+          {!artistId && (
+            <Link to="">
+              <p className="mt-2 text-base text-gray-400">
+                {songData?.subtitle}
+              </p>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DetailsHeader;
