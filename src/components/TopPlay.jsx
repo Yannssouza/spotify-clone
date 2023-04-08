862e4c04cbe0909b3df0545b439a2f4e26203115
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-04-08 08:54:57 -0300

    TopPlay.v1

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,3 +1,49 @@
-const TopPlay = () => <div>TopPlay</div>;
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import "swiper/css";
+import "swiper/css/free-mode";
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const topPlays = data?.tracks.slice(0, 5);
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div
+      ref={divRef}
+      className="ml-0 mb-6 flex max-w-full flex-1 flex-col xl:ml-6 xl:mb-0 xl:max-w-[500px]"
+    >
+      <div className="flex w-full flex-col">
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="text-2xl font-bold text-white">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="cursor-pointer text-base text-gray-300">See more</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
