50c28910dc2bee6fe42c64a7d937e23bddadf488
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-04-10 16:39:34 -0300

    AroundYou

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -25,6 +25,9 @@ export const shazamCoreApi = createApi({
     getArtistDetails: builder.query({
       query: (artistId) => `artists/get-top-songs?id=${artistId}`,
     }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `charts/track?locale=${countryCode}`,
+    }),
   }),
 });
 
@@ -33,4 +36,5 @@ export const {
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi;
